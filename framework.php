4274da485b3b58fb4594ebe2a79610660edd0443
AlisonYoon <politescarlett1@gmail.com>
2019-09-24 10:53:04 +0100

    php frameworks pros and cons

---
+++ framework.php
@@ -0,0 +1,29 @@
+<?php
+
+#framework - provide structure of your application (Framework could have many libraries you can use with it)
+#library - is just a tool you could use
+
+//PHP frameworks provides an architecture. that's the difference between PHP frameworks and other language's framework.
+//90% of the PHP frameworks enforces MVC architecture.
+
+//there are opinionated and un-opinionated frameworks
+//Opinionated enforces specifics how you have to structure your app
+//Un-opinionated is flexible, provides lots of options
+
+//Pros for opinionated framework :
+//  - enforces consistency
+//  - you just need to learn one thing. no decision needs to be made
+//  - it's easier to interpret (because you just need to learn one way)
+//  - More change that newly joined developer can start working right away
+
+//Cons for opinionated framework :
+//  - there's not flexibility (even if something is difficult to do it this way, you must do it this way)
+//  - generally learning curve is steeper
+//  - generally the size is bigger (more files, longer codes) - potentially it could make it slower but not necessarily.
+
+//Pros for Un-opinionated framework :
+//  - light weight (smaller, faster), flexible (different ways to solve problems)
+//  - it's harder to enforce consistency
+//  - more room for discussion could mean slow down the dev process
+//  - the chance is slimmer to find a developer who does the way that's exactly the way we do it
+
