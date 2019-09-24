333d978a670b82ee68e43939a23310374aa1be0c
AlisonYoon <politescarlett1@gmail.com>
2019-09-24 15:57:37 +0100

    Factory solves DI problem and DIC solves Factory problem

---
+++ DIC.php
@@ -0,0 +1,9 @@
+<?php
+
+# DIC : Dependency Injection Container
+// Factory got problems - it could create many same instances in different places
+// Factory tree - one factory could use many other factories and they could also use many other factories
+// DIC is designed to fix this problem.
+// One centralized repository of all the results of all the factories. - This way, you know that there is only one result of each factory exists.
+// You don't add factories in the DIC, you just "run" the factories in DIC.
+
--- dependency-injection-dependency-inversion.php
+++ dependency-injection-dependency-inversion.php
@@ -41,15 +41,3 @@ class barn {
 }
  *
  * */
-
-# Factory pattern solves the problems Dependency Injection has.
-// A Factory is generally an object which knows how to instantiate other objects for you.
-// You tell Factory to "give me UserModel", and the Factory goes and gets all the dependencies UserModel needs and return it to you. (I don't have to pass anything in UserModel)
-// Every time you create a class that has dependency, you create a Factory too.
-// It achieves encapsulation. For me, I don't need to know what factory does as long as it does what it's supposed to do.
-// Factory is not gonna follow the rule of Dependency Injection (it will instantiate objects inside) to help us get Dependency Injection.
-// Factory should be something I can run. (ie. named function, or class)
-// Different Frameworks have different opinions over Factory
-
-
-
