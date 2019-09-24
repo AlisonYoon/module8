66b3ce2b68901f136abbba27677e0affb20c5d67
AlisonYoon <politescarlett1@gmail.com>
2019-09-24 16:59:18 +0100

    what is DIC

--- DIC.php
+++ DIC.php
@@ -6,4 +6,4 @@
 // DIC is designed to fix this problem.
 // One centralized repository of all the results of all the factories. - This way, you know that there is only one result of each factory exists.
 // You don't add factories in the DIC, you just "run" the factories in DIC.
-
+// Now every part of your application uses DIC
